c8eceb9a87c3712e00dba44e429ab5eda282475c
qiaozi888@gmail.com <qiaozi888@gmail.com@b1398349-639f-7882-69d6-f984d5fa7213>
2014-04-22 12:05:40 +0000

    Initial source code import

---
+++ fineline-iepre10/src/fineline-iepre10.h
@@ -0,0 +1,246 @@
+/*  Copyright 2014 Derek Chadwick
+ 
+    This file is part of the FineLine Computer Forensics Timeline Tools.
+
+    FineLine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FineLine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FineLine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+   fineline-iepre10.h
+
+   Title : FineLine Computer Forensics Internet Explorer Cache Parser
+   Author: Derek Chadwick
+   Date  : 02/03/2014
+
+   Purpose: FineLine global definitions.
+
+*/
+
+#ifdef LINUX_BUILD
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+#else
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#endif
+
+#define HAVE_LOCAL_LIBCERROR
+#define HAVE_LOCAL_LIBCSTRING
+#define HAVE_LOCAL_LIBFDATETIME
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <libmsiecf.h>
+#include <libcerror_definitions.h>
+#include <libcerror_error.h>
+#include <libcerror_system.h>
+#include <libcerror_types.h>
+
+
+#include "uthash.h"
+
+/*
+   Constant Definitions
+*/
+
+#define DEBUG 0
+
+#define GUI_SERVER_PORT_STRING "58989"
+
+#define FL_PATH_MAX 4096 /* Redefine max path length since limits.h does weird things! */
+#define FL_MAX_INPUT_STR 4096
+#define FL_IP_ADDR_MAX 128
+#define MAX_EVENT_DESC_SIZE 256
+#define MAX_EVENT_ID_SIZE 8
+
+#define FL_FILE_OUT  0x01
+#define FL_GUI_OUT   0x02
+#define FL_INDEX_IN  0x04  /* This is the IE1-9 index.dat */
+#define FL_CACHE_IN  0x08  /* This is the IE10+ WebCacheV01.dat */
+#define FL_FILTER_ON 0x10
+
+#define DATABASE_FILE_EXT ".txt"
+#define EVENT_FILE_EXT ".fle"
+#define FL_URL_FILTER_LIST "fl-url-filter-list.txt"
+
+#ifdef LINUX_BUILD
+#define PATH_SEPARATOR "/"
+#define CURRENT_DIR "./"
+#define CONFIG_FILE "./fineline-linux.conf"
+#define LOG_FILE "./fineline-linux.log"
+#define DATABASE_FILE "./fineline-event-linux"
+#define BINARY_FILE "./fineline"
+#define MESSAGE_LOG_FILE "/var/log/messages"
+#define SECURITY_LOG_FILE "/var/log/security"
+#define EVENT_FILE "fineline-events"
+#define EVENT_LOG_PATH "./"
+#define IE_CACHE_PATH "./"
+#define IE_CACHE_FILE "index.dat"
+#else
+#define PATH_SEPARATOR "\\"
+#define CURRENT_DIR ".\\"
+#define CONFIG_FILE ".\\fineline.conf"
+#define LOG_FILE ".\\fineline.log"
+#define DATABASE_FILE "fineline-events"
+#define EVENT_FILE "fineline-events"
+#define IE_CACHE_PATH "C:\\Windows\\System32\\winevt\\Logs"
+#define IE_CACHE_FILE "index.dat"
+#endif /* LINUX_BUILD */
+
+/*
+   REGEXs
+*/
+
+#define BEST_REGEX "/^.*(?=.{6,})(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$/"
+#define STRONG_REGEX "/^[a-zA-Z\d\W_]*(?=[a-zA-Z\d\W_]{6,})(((?=[a-zA-Z\d\W_]*[A-Z])(?=[a-zA-Z\d\W_]*[\d]))|((?=[a-zA-Z\d\W_]*[A-Z])(?=[a-zA-Z\d\W_]*[\W_]))|((?=[a-zA-Z\d\W_]*[\d])(?=[a-zA-Z\d\W_]*[\W_])))[a-zA-Z\d\W_]*$/"
+#define WEAK_REGEX "/^[a-zA-Z\d\W_]*(?=[a-zA-Z\d\W_]{6,})(?=[a-zA-Z\d\W_]*[A-Z]|[a-zA-Z\d\W_]*[\d]|[a-zA-Z\d\W_]*[\W_])[a-zA-Z\d\W_]*$/"
+#define BAD_REGEX "/^((^[a-z]{6,}$)|(^[A-Z]{6,}$)|(^[\\d]{6,}$)|(^[\\W_]{6,}$))$/"
+
+/*
+STRUCTS - Windows event log data structures
+*/
+
+
+
+struct fl_project_header
+{
+   char *name;
+   char *investigator;
+   char *summary;
+   char *start_date;
+   char *end_date;
+   char *description;
+   int event_count;
+};
+
+typedef struct fl_project_header fl_project_header_t;
+
+struct fl_url_record
+{
+   int id;
+   double url_primary_time;
+   double url_checked_time;
+   char url_primary_time_string[48];
+   char url_checked_time_string[48];
+   char url_location_string[256];
+   char url_filename_string[256];
+   char url_record_string[FL_MAX_INPUT_STR];
+   UT_hash_handle hh;
+};
+
+typedef struct fl_url_record fl_url_record_t;
+
+struct fl_url_filter
+{
+   int url_id;
+   char url_string[FL_MAX_INPUT_STR];
+   UT_hash_handle hh;
+};
+
+typedef struct fl_url_filter fl_url_filter_t;
+
+/*
+   ENUMs
+*/
+
+enum op_modes { FL_DB_MODE = 1, FL_DB_MODE_X, FL_GUI_MODE, FL_GUI_MODE_X, FL_GUI_AND_DB_MODE, FL_GUI_AND_DB_MODE_X };
+enum error_codes { SUCCESS, FILE_ERROR, INTEGRITY_ERROR, MALLOC_ERROR, SYSTEM_ERROR, UNKNOWN_ANOMALY };
+enum log_modes { LOG_ERROR, LOG_WARNING, LOG_INFO };
+
+/*
+   Function Prototypes
+*/
+
+/* fineline-ie.c */
+int parse_command_line_args(int argc, char *argv[], char *fl_filename, char *in_file, char *gui_ip_address, char *filter_filename);
+
+/* fleventfile.c */
+FILE *open_fineline_event_file(char *event_file_name);
+int close_fineline_event_file(FILE *evt_file);
+int write_fineline_project_header(char *pstr, FILE *evt_file, int record_count);
+int write_fineline_event_record(char *estr, FILE *evt_file);
+
+/* flutil.c */
+int fatal(char *str);
+void *xcalloc (size_t size);
+void *xmalloc (size_t size);
+void *xrealloc (void *ptr, size_t size);
+int xfree(char *buf, int len);
+int print_help();
+char* xitoa(int value, char* result, int len, int base);
+int get_time_string(char *tstr, int slen);
+int validate_ipv4_address(char *ipv4_addr);
+int validate_ipv6_address(char *ipv6_addr);
+char *ltrim(char *s);
+char *rtrim(char *s);
+char *trim(char *s);
+
+/* fllog.c */
+int open_log_file(char *startup_path);
+int print_log_entry(char *estr);
+int close_log_file();
+
+/* flieindexparser.c */
+int parse_ie_index_file(char *iecfile, char *fl_event_filename, int mode, char *gui_ip_addr, char *filter_filename);
+int process_url_item(libmsiecf_item_t *url_item, int id, int mode);
+int format_url_event_string(struct fl_url_record *flurl);
+
+/* flfilterhashmap.c */
+
+int load_url_filters(char *filter_filename);
+struct fl_url_filter *find_filter(int url_id);
+void add_filter(int url_id, struct fl_url_filter *flurl);
+int match_url_filter(char *url_string);
+
+/* flurlhashmap.c */
+
+void add_url_record(uint64_t url_id, struct fl_url_record *flurl);
+struct fl_url_record *find_url(uint64_t url_id);
+void write_url_map(FILE *outfile);
+void write_url_map_in_time_sequence(FILE *outfile, uint64_t lowest);
+void send_url_map();
+void send_url_map_in_time_sequence(uint64_t lowest);
+int primary_time_sort(struct fl_url_record *a, struct fl_url_record *b);
+int checked_time_sort(struct fl_url_record *a, struct fl_url_record *b);
+void sort_by_primary_time();
+void sort_by_checked_time();
+void sort_by_record_number();
+void delete_event(struct fl_url_record *url_record);
+void delete_all();
+struct fl_url_record *get_first_url_record();
+struct fl_url_record *get_last_url_record();
+uint64_t get_first_record_number();
+
+/* flsocket.c */
+
+int init_socket(char *gui_ip_address);
+int send_event(char *event_string);
+char *get_response();
+int close_socket();
+
+
+/* Unit Test Functions */
+int unit_tests(FILE *log_file);
+
+
+
---
+++ fineline-iepre10/src/fleventfile.c
@@ -0,0 +1,160 @@
+
+/*  Copyright 2014 Derek Chadwick
+ 
+    This file is part of the FineLine Computer Forensics Timeline Tools.
+
+    FineLine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FineLine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FineLine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+   fleventfile.c
+
+   Title : FineLine Computer Forensics Timeline Constructor
+   Author: Derek Chadwick
+   Date  : 22/12/2013
+
+   Purpose: fineline event file creation and writing.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include "fineline-iepre10.h"
+
+/*
+   Function: open_event_file()
+
+   Purpose : Creates the event record event file in the current working directory.
+   Input   : event filename.
+   Output  : Returns event file pointer.
+*/
+FILE *open_fineline_event_file(char *evt_file_name)
+{
+    FILE *event_file;
+    event_file = fopen(evt_file_name, "a");
+    if (event_file == NULL)
+    {
+       printf("open_fineline_event_file() <ERROR>: could not open event file: %s\n", evt_file_name);
+    }
+    printf("open_event_file() <INFO> open_fineline_event_file(): %s\n", evt_file_name);
+
+   return(event_file);
+}
+
+
+/* DEPRECATED: format superseded.
+   Function: write_fineline_event_record()
+
+   Purpose : Creates an event string and prints to the fineline event file and stdout.
+           :
+   Input   : Event string, event file and log file.
+   Output  : Timestamped event entry.
+*/
+int write_fineline_event_record(char *estr, FILE *evt_file)
+{
+   time_t curtime;
+   struct tm *loctime;
+   int slen = strlen(estr) + 100;
+   char *event = (char *)xcalloc(slen);
+   char *time_str;
+
+   /* Get the current time. */
+   curtime = time (NULL);
+   loctime = localtime (&curtime);
+
+   time_str = asctime(loctime);
+   strncpy(event, time_str, strlen(time_str) - 1);
+   strncat(event, " ", 1);
+   strncat(event, estr, slen);
+
+   fputs (event, evt_file);
+
+   printf("%s", event);
+
+   xfree(event, slen);
+
+   return(0);
+}
+
+/*
+   Function: write_fineline_project_header()
+
+   Purpose : Creates an event log header string and prints to the event file.
+           :
+   Input   : Log header string, event file and log file.
+   Output  : Timestamped log header entry.
+*/
+int write_fineline_project_header(char *pstr, FILE *evt_file, int record_count)
+{
+   time_t curtime;
+   struct tm *loctime;
+   int slen = strlen(pstr) + FL_MAX_INPUT_STR;
+   char *hdr = (char *) xcalloc(slen);
+   char *time_str;
+   char start_date_time_string[32];
+   char end_date_time_string[32];
+   /* struct fl_event_record *fler; */
+
+   memset(start_date_time_string, 0, 32);
+   memset(end_date_time_string, 0, 32);
+
+   strncpy(start_date_time_string, "NONE", 4);
+   strncpy(end_date_time_string, "NONE", 4);
+
+   /* Get the current time. */
+   curtime = time (NULL);
+   loctime = localtime (&curtime);
+   time_str = asctime(loctime);
+
+   /* Get the date of the first and last event records and use these to set the project start/end dates 
+   fler = get_first_event_record();
+   if (fler != NULL)
+   {
+      strncpy(start_date_time_string, fler->event_time_string, 32);
+   }
+   fler = get_last_event_record();
+   if (fler != NULL)
+   {
+      strncpy(end_date_time_string, fler->event_time_string, 32);
+   }
+   */
+
+   /* First and last records are same day because event file is a circular buffer with a fixed maximum size */
+   /* TODO: determine a method of getting the last date/time stamp from the event file */
+
+   strcpy(hdr, "<project><name>FineLine Project ");
+   strncat(hdr, time_str, strlen(time_str) - 1);
+   strcat(hdr, "</name><investigator>NONE</investigator><summary>NONE</summary><startdate>");
+   strncat(hdr, start_date_time_string, 32);
+   strcat(hdr,"</startdate><enddate>");
+   strncat(hdr, end_date_time_string, 32);
+   strcat(hdr, "</enddate><description>");
+   strncat(hdr, pstr, slen);
+   strcat(hdr, "</description></project>\n");
+   fputs (hdr, evt_file);
+
+   print_log_entry("write_fineline_project_header() <INFO> Wrote Project Header.\n");
+
+   xfree(hdr, slen);
+
+   return(0);
+}
+
+
+
